fe3127ece8c95502f5e7255b02cdc674d5aac69d
enahemamuti14 <enahemamuti@gmail.com>
2022-06-04 09:59:11 +0700

    haloo

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get('/', function (req, res) {
     npm: '2042028'
   });
 })
+app.post('/', function (req, res) {
+  res.json({
+    number: 1,
+    nama: 'Mutiara Ratna Enahema',
+    npm: '2042028'
+  });
+})
 
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
